230afe17be6f5ee845ed1f4a1ae6ce4871f24cf3
Osama-Moh <usama.nasser21@gmail.com>
2024-10-30 02:40:49 +0300

    complete more TODOs in shader.hpp

--- source/common/shader/shader.hpp
+++ source/common/shader/shader.hpp
@@ -45,7 +45,7 @@ namespace our {
 
         void set(const std::string &uniform, GLuint value) {
             //TODO: (Req 1) Send the given unsigned integer value to the given uniform
-
+            glUniform1ui(getUniformLocation(uniform), value);
         }
 
         void set(const std::string &uniform, GLint value) {
@@ -55,18 +55,22 @@ namespace our {
 
         void set(const std::string &uniform, glm::vec2 value) {
             //TODO: (Req 1) Send the given 2D vector value to the given uniform
+            glUniform2f(getUniformLocation(uniform), value[0], value[1]);
         }
 
         void set(const std::string &uniform, glm::vec3 value) {
             //TODO: (Req 1) Send the given 3D vector value to the given uniform
+            glUniform3f(getUniformLocation(uniform), value[0], value[1], value[2]);
         }
 
         void set(const std::string &uniform, glm::vec4 value) {
             //TODO: (Req 1) Send the given 4D vector value to the given uniform
+            glUniform4f(getUniformLocation(uniform), value[0], value[1], value[2], value[3]);
         }
 
         void set(const std::string &uniform, glm::mat4 matrix) {
             //TODO: (Req 1) Send the given matrix 4x4 value to the given uniform
+            glUniformMatrix4fv(getUniformLocation(uniform), 1, GL_FALSE, glm::value_ptr(matrix));
         }
 
         //TODO: (Req 1) Delete the copy constructor and assignment operator.
