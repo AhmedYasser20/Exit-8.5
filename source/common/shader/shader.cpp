9f0cc1f265438e5c6dc0840870cf8147b511632f
Osama-Moh <usama.nasser21@gmail.com>
2024-11-24 18:15:39 +0200

    finished shader.cpp

--- source/common/shader/shader.cpp
+++ source/common/shader/shader.cpp
@@ -25,7 +25,22 @@ bool our::ShaderProgram::attach(const std::string &filename, GLenum type) const 
     // an error in the given shader. You should use it to check if there is a
     // compilation error and print it so that you can know what is wrong with
     // the shader. The returned string will be empty if there is no errors.
+    GLuint shader = glCreateShader(type);
 
+    glShaderSource(shader, 1, &sourceCStr, nullptr);
+
+    glCompileShader(shader);
+
+    std::string error = checkForShaderCompilationErrors(shader);
+    if (!error.empty()) {
+        std::cerr << "ERROR: Couldn't compile shader: " << filename << std::endl;
+        std::cerr << error << std::endl;
+        glDeleteShader(shader);
+        return false;
+    }
+    glAttachShader(program, shader);
+    glDeleteShader(shader);
+    
     //We return true if the compilation succeeded
     return true;
 }
@@ -38,7 +53,14 @@ bool our::ShaderProgram::link() const {
     // an error in the given program. You should use it to check if there is a
     // linking error and print it so that you can know what is wrong with the
     // program. The returned string will be empty if there is no errors.
-
+    
+    glLinkProgram(program);
+    std::string error = checkForLinkingErrors(program);
+    if (!error.empty()) {
+        std::cerr << "ERROR: Couldn't link shader program" << std::endl;
+        std::cerr << error << std::endl;
+        return false;
+    }
     return true;
 }
 
