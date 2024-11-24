a5c0afd981a00df9e6ec6004d99b93f556d9faaa
Osama-Moh <usama.nasser21@gmail.com>
2024-11-24 20:48:43 +0200

    Req 2 tested and done

--- source/common/mesh/mesh.hpp
+++ source/common/mesh/mesh.hpp
@@ -30,31 +30,31 @@ namespace our {
         Mesh(const std::vector<Vertex>& vertices, const std::vector<unsigned int>& elements)
         {
             //TODO: (Req 2) Write this function
-            glGenBuffers(1, &VBO);//
-            glBindBuffer(GL_ARRAY_BUFFER, VBO);//
+
+            glGenVertexArrays(1, &VAO);
+            glBindVertexArray(VAO);
+            
+            glGenBuffers(1, &VBO);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
             glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
 
-            elementCount = elements.size();
-            glGenBuffers(1, &EBO);//
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);//
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, elements.size() * sizeof(unsigned int), elements.data(), GL_STATIC_DRAW);
+            elementCount = static_cast<GLsizei>(elements.size());
 
-            glGenVertexArrays(1, &VAO);//
-            glBindVertexArray(VAO);//
+            glGenBuffers(1, &EBO);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementCount * sizeof(unsigned int), elements.data(), GL_STATIC_DRAW);
 
-            glVertexAttribPointer(ATTRIB_LOC_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));//
-            glEnableVertexAttribArray(ATTRIB_LOC_POSITION);//
+            glVertexAttribPointer(ATTRIB_LOC_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
+            glEnableVertexAttribArray(ATTRIB_LOC_POSITION);
 
-            glVertexAttribPointer(ATTRIB_LOC_COLOR, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vertex), (void*)offsetof(Vertex, color));//
-            glEnableVertexAttribArray(ATTRIB_LOC_COLOR);//
+            glVertexAttribPointer(ATTRIB_LOC_COLOR, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vertex), (void*)offsetof(Vertex, color));
+            glEnableVertexAttribArray(ATTRIB_LOC_COLOR);
 
+            glVertexAttribPointer(ATTRIB_LOC_TEXCOORD, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, tex_coord));
+            glEnableVertexAttribArray(ATTRIB_LOC_TEXCOORD);
 
-            glVertexAttribPointer(ATTRIB_LOC_TEXCOORD, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, tex_coord));//
-            glEnableVertexAttribArray(ATTRIB_LOC_TEXCOORD);//
-
-            glVertexAttribPointer(ATTRIB_LOC_NORMAL, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));//
-            glEnableVertexAttribArray(ATTRIB_LOC_NORMAL);//
-            
+            glVertexAttribPointer(ATTRIB_LOC_NORMAL, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+            glEnableVertexAttribArray(ATTRIB_LOC_NORMAL);
             
             glBindVertexArray(0);
             
@@ -67,7 +67,7 @@ namespace our {
         {
             //TODO: (Req 2) Write this function
             glBindVertexArray(VAO);
-            glDrawElements(GL_TRIANGLES, elementCount, GL_UNSIGNED_BYTE, 0);
+            glDrawElements(GL_TRIANGLES, elementCount, GL_UNSIGNED_INT, 0);
             glBindVertexArray(0);
         }
 
